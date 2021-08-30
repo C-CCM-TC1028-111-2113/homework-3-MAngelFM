a9dc66daed9a87a1d09ce1234b29f853e01df4bc
MAngelFM <88851006+MAngelFM@users.noreply.github.com>
2021-08-29 21:30:54 -0500

    Update exercise.py

--- assignments/08TarjeteriaEspanola/src/exercise.py
+++ assignments/08TarjeteriaEspanola/src/exercise.py
@@ -1,6 +1,21 @@
 
 def main():
     #escribe tu código abajo de esta línea
+    def pliego(x,y):
+    uno = x * 12
+    dos = y * 35
+
+    if uno < dos:
+        print("El número maxiomo de tarjetas que se pueden hacer es: ",uno)
+    else:
+        print("El número maximo de tarjetas que se pueden hacer es: ",dos)
+
+
+    pli = int(input("Dame la cantidad de pliegos de papel albanene: "))
+    plu = int(input("Dame la cantidad de plumones: "))
+
+    pliego(pli,plu)
+
     pass
 
 if __name__=='__main__':
